954eb05ddc3a594a9db2201ed3caf56d4b9ad5ee
mateusz <przykladowy@o3.pl>
2020-02-18 21:26:00 +0100

    initial for angular and spring

---
+++ spring/src/main/java/com/zipcompany/gamex/ExampleController.java
@@ -0,0 +1,19 @@
+package com.zipcompany.gamex;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// TODO
+//  create local database, uncomment data jpa in pom.xml and fix application.properties
+
+
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/hello")
+    public String helloFromExampleController() {
+        return "hello from example controller";
+    }
+}
---
+++ spring/src/main/java/com/zipcompany/gamex/GamexApplication.java
@@ -0,0 +1,13 @@
+package com.zipcompany.gamex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamexApplication.class, args);
+    }
+
+}
